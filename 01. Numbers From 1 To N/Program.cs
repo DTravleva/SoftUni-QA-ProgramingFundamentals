598fd611a21ae0ca7fa6ccade04852506984ea46
DTravleva <39096928+DTravleva@users.noreply.github.com>
2023-09-28 14:28:25 +0300

    Add files via upload

---
+++ 01. Numbers From 1 To N/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace _01._Numbers_From_1_To_N
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= n; i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
